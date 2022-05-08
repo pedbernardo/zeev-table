3fb9d882a1d79399ff15e1579b6ff228042e78f8
Pedro Bernardo <pedrovbernardo@gmail.com>
2022-05-08 20:16:43 -0300

    feat: add config and entry file

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const NAME = 'TableMv'
+
+export const BUTTONS_REFS = {
+  btnInsert: '#BtnInsertNewRow',
+  btnDelete: '.btn-delete-mv'
+}
+
+export const HELPER_CLASSES = {
+  disabledClass: '-disabled',
+  readonlyClass: '-readonly'
+}
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import { createTable } from './instance'
+
+import {
+  reset,
+  appendData,
+  enable,
+  disable
+} from './utils/actions'
+
+import {
+  getRows,
+  getLastRow,
+  getLength,
+  getData,
+  getRowData
+} from './utils/dom'
+
+export {
+  // instance
+  createTable,
+
+  // actions
+  reset,
+  appendData,
+  enable,
+  disable,
+
+  // dom
+  getRows,
+  getLastRow,
+  getLength,
+  getData,
+  getRowData
+}
